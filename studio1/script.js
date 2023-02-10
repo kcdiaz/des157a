d0968a557da989497579d286d41cdcb203d352dd
kcdiaz <101031979+kcdiaz@users.noreply.github.com>
2023-02-10 14:59:21 -0800

    Update script.js

--- studio1/script.js
+++ studio1/script.js
@@ -1,18 +1,70 @@
-const form = document.querySelector("#bakery-form");
+(function() {
+    "use strict";
+    console.log("reading js");
 
+    // Selecting the form and submit button
+const form = document.querySelector("#myform");
+const submitBtn = document.querySelector("#submitBtn");
+
+// Selecting the overlay
+const overlay = document.querySelector("#overlay");
+const backBtn = document.querySelector("#backBtn");
+const aTimeBlank = document.querySelector("#aTimeBlank");
+const adjBlank = document.querySelector("#adjBlank");
+const aIngredientBlank = document.querySelector("#aIngredientBlank");
+const colorBlank1 = document.querySelector("#colorBlank1");
+const decorBlank1 = document.querySelector("#decorBlank1");
+const pastryDecor = document.querySelector("#pastryDecor");
+
+// Adding a submit event listener to the form
 form.addEventListener("submit", function(event) {
-  // Prevent the form from submitting and reloading the page
-  event.preventDefault();
+    // Preventing the default behavior of the form submit
+    event.preventDefault();
 
-  // Get an array of all checked items
-  const checkedItems = Array.from(document.querySelectorAll("input:checked"));
+    // Getting the values from the form
+    const aTime = document.querySelector("#aTime").value;
+    const adj = document.querySelector("#adj").value;
+    const aIngredient = document.querySelector("#aIngredient").value;
+    const decoration = document.querySelector('input[name="decoration"]:checked').value;
 
-  // Extract the value of each checked item and store it in an array
-  const selectedItems = checkedItems.map(item => item.value);
+    // Updating the content in the overlay
+    aTimeBlank.innerHTML = aTime;
+    adjBlank.innerHTML = adj;
+    aIngredientBlank.innerHTML = aIngredient;
+    switch (decoration) {
+        case "pink":
+            colorBlank1.innerHTML = "Pink";
+            decorBlank1.innerHTML = "Pink";
+            pastryDecor.src = "images/pinkset.png";
+            break;
+        case "purple":
+            colorBlank1.innerHTML = "Purple";
+            decorBlank1.innerHTML = "Purple";
+            pastryDecor.src = "images/purpleset.png";
+            break;
+        case "blue":
+            colorBlank1.innerHTML = "Blue";
+            decorBlank1.innerHTML = "Blue";
+            pastryDecor.src = "images/blueset.png";
+            break;
+        case "green":
+            colorBlank1.innerHTML = "Green";
+            decorBlank1.innerHTML = "Green";
+            pastryDecor.src = "images/greenset.png";
+            break;
+        case "mystery":
+            colorBlank1.innerHTML = "Mystery";
+            decorBlank1.innerHTML = "Mystery";
+            pastryDecor.src = "images/mysteryset.png";
+            break;
+    }
 
-  // Store the selected items in local storage to access it on the next page
-  localStorage.setItem("selectedItems", JSON.stringify(selectedItems));
+    // Showing the overlay
+    overlay.classList.remove("hidden");
+});
 
-  // Redirect to the next page
-  window.location.href = "box-of-pastries.html";
+// Adding a click event listener to the back button in the overlay
+backBtn.addEventListener("click", function() {
+    // Hiding the overlay
+    overlay.classList.add("hidden");
 });
