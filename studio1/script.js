657870e8aff3a207a2365a6c31b2837e645d24ff
kcdiaz <101031979+kcdiaz@users.noreply.github.com>
2023-02-11 23:06:18 -0800

    Update script.js

--- studio1/script.js
+++ studio1/script.js
@@ -1,70 +1,49 @@
 (function() {
-    "use strict";
-    console.log("reading js");
+  "use strict";
+  console.log("reading js");
 
-    // Selecting the form and submit button
-const form = document.querySelector("#myform");
-const submitBtn = document.querySelector("#submitBtn");
-
-// Selecting the overlay
-const overlay = document.querySelector("#overlay");
-const backBtn = document.querySelector("#backBtn");
-const aTimeBlank = document.querySelector("#aTimeBlank");
-const adjBlank = document.querySelector("#adjBlank");
-const aIngredientBlank = document.querySelector("#aIngredientBlank");
-const colorBlank1 = document.querySelector("#colorBlank1");
-const decorBlank1 = document.querySelector("#decorBlank1");
-const pastryDecor = document.querySelector("#pastryDecor");
-
-// Adding a submit event listener to the form
-form.addEventListener("submit", function(event) {
-    // Preventing the default behavior of the form submit
-    event.preventDefault();
-
-    // Getting the values from the form
-    const aTime = document.querySelector("#aTime").value;
-    const adj = document.querySelector("#adj").value;
-    const aIngredient = document.querySelector("#aIngredient").value;
-    const decoration = document.querySelector('input[name="decoration"]:checked').value;
-
-    // Updating the content in the overlay
-    aTimeBlank.innerHTML = aTime;
-    adjBlank.innerHTML = adj;
-    aIngredientBlank.innerHTML = aIngredient;
-    switch (decoration) {
-        case "pink":
-            colorBlank1.innerHTML = "Pink";
-            decorBlank1.innerHTML = "Pink";
-            pastryDecor.src = "images/pinkset.png";
-            break;
-        case "purple":
-            colorBlank1.innerHTML = "Purple";
-            decorBlank1.innerHTML = "Purple";
-            pastryDecor.src = "images/purpleset.png";
-            break;
-        case "blue":
-            colorBlank1.innerHTML = "Blue";
-            decorBlank1.innerHTML = "Blue";
-            pastryDecor.src = "images/blueset.png";
-            break;
-        case "green":
-            colorBlank1.innerHTML = "Green";
-            decorBlank1.innerHTML = "Green";
-            pastryDecor.src = "images/greenset.png";
-            break;
-        case "mystery":
-            colorBlank1.innerHTML = "Mystery";
-            decorBlank1.innerHTML = "Mystery";
-            pastryDecor.src = "images/mysteryset.png";
-            break;
-    }
-
-    // Showing the overlay
-    overlay.classList.remove("hidden");
-});
-
-// Adding a click event listener to the back button in the overlay
-backBtn.addEventListener("click", function() {
-    // Hiding the overlay
-    overlay.classList.add("hidden");
+  document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("#myform");
+    const overlay = document.querySelector("#overlay");
+    const backBtn = document.querySelector("#backBtn");
+    const aTimeBlank = document.querySelector("#aTimeBlank");
+    const adjBlank = document.querySelector("#adjBlank");
+    const aIngredientBlank = document.querySelector("#aIngredientBlank");
+    const decorationBlank = document.querySelector("#decorationBlank");
+    const noteBlank = document.querySelector("#noteBlank");
+    const pastryDecor = document.querySelector("#pastryDecor");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const aTime = document.querySelector("#aTime").value;
+      const adj = document.querySelector("#adj").value;
+      const aIngredient = document.querySelector("#aIngredient").value;
+      const decoration = document.querySelector('input[name="decoration"]:checked').value;
+      const note = document.querySelector("#note").value;
+  
+      aTimeBlank.innerHTML = aTime;
+      adjBlank.innerHTML = adj;
+      aIngredientBlank.innerHTML = aIngredient;
+      noteBlank.innerHTML = note;
+      decorationBlank.innerHTML = decoration;
+  
+      if (decoration === "pink") {
+        pastryDecor.src = "images/pinkset.png";
+      } else if (decoration === "purple") {
+        pastryDecor.src = "images/purpleset.png";
+      } else if (decoration === "blue") {
+        pastryDecor.src = "images/blueset.png";
+      } else if (decoration === "green") {
+        pastryDecor.src = "images/greenset.png";
+      } else if (decoration === "mystery") {
+        pastryDecor.src = "images/mysteryset.png";
+      }
+  
+      overlay.classList.remove("hidden");
+    });
+  
+    backBtn.addEventListener("click", function() {
+      overlay.classList.add("hidden");
+    });
 });
